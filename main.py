638bea68fc5fbfe47d177594fb0db918d570cb80
Salman Nazeer <salmanselu2002@gmail.com>
2021-06-23 17:24:16 +0530

    add br tag

--- main.py
+++ main.py
@@ -2,30 +2,38 @@
 
 command = input(">>")
 markdown = open(command, 'r')
-html = open(command[:-2]+'html', 'a')
+html = open(command[:-2]+'html', 'w')
 lines = markdown.readlines()
 
-for line in lines:
+for i in range(len(lines)):
 # Header Tags:
-    if line[0] == '#':
-        if '######' in line:
-            line = line.replace('######', '<h6>')
-            line += '<h6>'
-        if '#####' in line:
-            line = line.replace('#####', '<h5>')
-            line += '<h5>'
-        if '####' in line:
-            line = line.replace('####', '<h4>')
-            line += '<h4>'
-        if '###' in line:
-            line = line.replace('###', '<h3>')
-            line += '<h3>'
-        if '##' in line:
-            line = line.replace('##', '<h2>')
-            line += '<h2>'
-        if '#' in line:
-            line = line.replace('#', '<h1>')
-            line += '<h1>'
-    html.write(line)
+    if lines[i][0] == '#':
+        if '######' in lines[i]:
+            lines[i] = lines[i].replace('\n', ' ')
+            lines[i] = lines[i].replace('######', '<h6>')
+            lines[i] += '</h6>'
+        elif '#####' in lines[i]:
+            lines[i] = lines[i].replace('\n', ' ')
+            lines[i] = lines[i].replace('#####', '<h5>')
+            lines[i] += '</h5>'
+        elif '####' in lines[i]:
+            lines[i] = lines[i].replace('\n', ' ')
+            lines[i] = lines[i].replace('####', '<h4>')
+            lines[i] += '</h4>'
+        elif '###' in lines[i]:
+            lines[i] = lines[i].replace('\n', ' ')
+            lines[i] = lines[i].replace('###', '<h3>')
+            lines[i] += '</h3>'
+        elif '##' in lines[i]:
+            lines[i] = lines[i].replace('\n', ' ')
+            lines[i] = lines[i].replace('##', '<h2>')
+            lines[i] += '</h2>'
+        elif '#' in lines[i]:
+            lines[i] = lines[i].replace('\n', ' ')
+            lines[i] = lines[i].replace('#', '<h1>')
+            lines[i] += '</h1>'
+
+    lines[i] = lines[i].replace('  \n', '<br>')
+html.writelines(lines)
 html.close()
 markdown.close()
